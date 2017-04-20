6fdd6fd1e214f94a5510a72ea09e5fe6d76d5991
deanaazy <deanaazy@gmail.com>
2017-04-20 13:59:44 +1000

    initial version

---
+++ src/main/java/com/asf/data/datareader/CsvReader.java
@@ -0,0 +1,13 @@
+package com.asf.data.datareader;
+
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVFormat;
+
+
+/**
+ * Created by AlexZhang on 2017/4/20.
+ */
+public class CsvReader {
+
+
+}
---
+++ src/main/java/com/asf/data/datareader/MysqlReader.java
@@ -0,0 +1,7 @@
+package com.asf.data.datareader;
+
+/**
+ * Created by AlexZhang on 2017/4/20.
+ */
+public class MysqlReader {
+}
