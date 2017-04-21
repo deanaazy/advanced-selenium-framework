a967c3b7b8401f3da6bc6ad7ad4a4040291a412e
deanaazy <deanaazy@gmail.com>
2017-04-21 14:20:06 +1000

    initial version

---
+++ src/main/java/com/asf/common/PropertyParser.java
@@ -0,0 +1,7 @@
+package com.asf.common;
+
+/**
+ * Created by AlexZhang on 2017/4/21.
+ */
+public class PropertyParser {
+}
--- src/main/java/com/asf/data/datareader/JsonReader.java
+++ src/main/java/com/asf/data/datareader/JsonReader.java
@@ -4,4 +4,6 @@ package com.asf.data.datareader;
  * Created by AlexZhang on 2017/4/20.
  */
 public class JsonReader {
+
+
 }
