c93eeae277dfdace35f346454b337e916c29020a
deanaazy <deanaazy@gmail.com>
2017-04-22 14:43:29 +1000

    initial version

---
+++ src/main/java/com/asf/data/database/connector/MysqlConnector.java
@@ -0,0 +1,7 @@
+package com.asf.data.database.connector;
+
+/**
+ * Created by AlexZhang on 2017/4/22.
+ */
+public class MysqlConnector {
+}
---
+++ src/main/java/com/asf/data/database/sql/SqlBuilder.java
@@ -0,0 +1,7 @@
+package com.asf.data.database.sql;
+
+/**
+ * Created by AlexZhang on 2017/4/22.
+ */
+public class SqlBuilder {
+}
